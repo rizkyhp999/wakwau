7965afbdb6c4c6efb5dfc900f7f7d2dd2ef2efc1
rizkyhp999 <rizkyhutamaputra@gmail.com>
2023-12-24 22:35:09 +0700

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,14 +3,18 @@ import React from "react";
 import Link from "next/link";
 export default function Navbar() {
   const handleHamburger = () => {
-    console.log("loooh");
     const hamburger = document.querySelector("#hamburger");
-    hamburger.classList.toggle("hamburger-active");
+    const nav = document.querySelector("#nav-menu");
+    if (hamburger != null && nav != null) {
+      hamburger.classList.toggle("hamburger-active");
+      nav.classList.toggle("hidden");
+    }
   };
+
   return (
     <>
       <header className="bg-primer absolute h-[60px] top-0 left-0 w-full flex items-center z-10">
-        <div className="container">
+        <div className="container mx-auto">
           <div className="flex items-center justify-between relative">
             <div className="px-4">
               <Link href="/" className="block ">
@@ -31,6 +35,16 @@ export default function Navbar() {
               </button>
             </div>
           </div>
+          <nav
+            id="nav-menu"
+            className="hidden bg-primer absolute h-[60px] top-full left-0 w-full items-center"
+          >
+            <ul>
+              <li>
+                <Link href="/">Beranda</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
     </>
