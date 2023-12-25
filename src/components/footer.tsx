74387e873f13116273ef11f620863411f15967f1
rizkyhp999 <rizkyhutamaputra@gmail.com>
2023-12-25 23:05:09 +0700

    add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function footer() {
+  return (
+    <footer className="bg-primer pt-14 pb-12 bottom-0 w-full flex items-center z-10 lg:px-[100px] ">
+      <div className="container mx-auto p-5 lg:item-center lg:justify-between lg:flex">
+        <div className="">
+          <div className="text-justify text-white text-xl font-bold  leading-relaxed">
+            Badan Pengembangan <br />
+            dan Pembinaan Bahasa
+          </div>
+          <div className="text-justify text-white text-base font-semibold  leading-9">
+            Jalan Daksinapati Barat IV, Rawamangun Jakarta Timur 13220
+            <br />
+            Telepon (021) 4706287, 4706288, 4896558, 4894546
+            <br />
+            Faksimile (021) 4750407
+            <br />
+            Pos-el badan.bahasa[at]kemdikbud.go.id
+          </div>
+        </div>
+        <div className="mt-12 lg:mt-0">
+          <div className="text-justify text-white text-xl font-bold  leading-relaxed">
+            Media Sosial
+          </div>
+          <div className="text-justify text-white text-base font-semibold  leading-9">
+            @badanbahasakemendikbud
+          </div>
+          <div className="text-justify text-white text-base font-semibold  leading-9">
+            @BadanBahasa
+          </div>
+          <div className="text-justify text-white text-base font-semibold  leading-9">
+            Badan Pengembangan dan Pembinaan Bahasa
+          </div>
+          <div className="text-justify text-white text-base font-semibold  leading-9">
+            Badan Pengembangan dan Pembinaan Bahasa
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
