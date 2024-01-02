79d27f6e5def1cebe5d0f4f467bea9cfc173362a
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-01-03 03:16:35 +0700

    text animasii

--- src/components/random/animasi.tsx
+++ src/components/random/animasi.tsx
@@ -44,7 +44,7 @@ export const Animasi = ({ children, width = "fit-content" }: Props) => {
         }}
         initial="hidden"
         animate={slideControls}
-        transition={{ duration: 0.5, ease: "easeIn" }}
+        transition={{ duration: 0.6, ease: "easeIn" }}
         style={{
           position: "absolute",
           top: 4,
