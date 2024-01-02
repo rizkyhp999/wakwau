d38448f398749e393231f2b03c30278f044c4d13
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-01-02 21:38:56 +0700

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Carousel from "@/components/carousel/carousel";
+import TextBeranda from "@/components/random/textBeranda";
 
 export default function Home() {
   return (
     <>
+      <div></div>
       <Carousel></Carousel>
-
-      <div className="container mx-auto">
+      <TextBeranda></TextBeranda>
+      <div className=" bg-primer mx-auto">
         <p>
           Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau
           Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau Wakwau
