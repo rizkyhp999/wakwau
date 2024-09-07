77dcd3a2b479cc4a0ecd9b5017383d066f2bbdf9
Fatih Mahawisesa <222011585@stis.ac.id>
2024-09-07 12:08:29 +0700

    tambah footer

--- src/layout/user/beranda/footer.tsx
+++ src/layout/user/beranda/footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function footer() {
-  return <div>footer</div>;
+  return <div className="border border-black">
+    FOOTER 2024 ©KOSALI WAKWAU TRYOUT CPNS JAYA
+  </div>;
 }
