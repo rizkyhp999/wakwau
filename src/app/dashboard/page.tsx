5e4738c905c2fd3e05b25ccc8822de17ae2d2235
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-09-09 23:53:49 +0700

    card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,51 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { EnvelopeOpenIcon, EnterIcon } from "@radix-ui/react-icons";
 
+import { Button } from "@/components/ui/button";
 export default function page() {
-  return <div>paaaage</div>;
+  return (
+    <div className="container flex flex-wrap md:flex-nowrap justify-center max-w-xl px-4 py-4 sm:px-6 lg:px-8">
+      <Card className="m-5">
+        <CardHeader>
+          <CardTitle className="text-3xl justify-center flex">
+            Mulai Tryout Sekarang
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="flex items-center justify-center">
+          <CardDescription className="text-justify mx-2">
+            Uji kemampuan Anda dengan tryout online terbaru dan tingkatkan
+            peluang sukses seleksi kedinasan mulai sekarang!
+          </CardDescription>
+        </CardContent>
+        <CardFooter className="flex items-center justify-center">
+          <Button>
+            <EnterIcon className="mr-2 h-4 w-4" /> Mulai
+          </Button>
+        </CardFooter>
+      </Card>
+      <Card className="m-5">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>
+            mampuan Anda kemampuan Anda kemampuanda kemampuanda kemampuanda
+            kemampuan Anda kemampuan Anda tent
+          </p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
--- src/layout/user/dashboard/navbar.tsx
+++ src/layout/user/dashboard/navbar.tsx
@@ -271,9 +271,7 @@ export default function Navbar({
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-          {children}
-        </main>
+        <main>{children}</main>
       </div>
     </div>
   );
